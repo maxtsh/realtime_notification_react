482961461a55b6656c5ceecf6a931c95d0a825d5
Mohammad Tat Shahdoost <tatshahdoost@gmail.com>
2023-03-23 03:39:13 +0430

    added basic socket features

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -1,8 +1,13 @@
+import { useEffect, useState } from "react";
+import { BsBell } from "react-icons/bs";
 import { clearFromLS, readFromLS } from "@/utils/Storage/LocalStorage";
 import { useNavigate } from "react-router-dom";
 import { Container } from "./Header.styles";
+import useSocket from "@/hooks/useSocket";
 
 const Header: React.FC = () => {
+  const [notifs, setNotifs] = useState(0);
+  const socket = useSocket();
   const navigate = useNavigate();
   const username = readFromLS("notification_username");
 
@@ -11,11 +16,30 @@ const Header: React.FC = () => {
     navigate("/login", { replace: true });
   };
 
+  useEffect(() => {
+    socket?.emit("NewUser", username);
+  }, [socket, username]);
+
+  useEffect(() => {
+    socket?.on("notification", ({ message }) => {
+      setNotifs((prev) => prev + 1);
+      console.log(message);
+    });
+  }, [socket]);
+
   return (
     <Container>
-      <h1>Header</h1>
-      <h3>Welcome {username}</h3>
-      <button onClick={logOut}>Logout</button>
+      <div className="left">
+        <h1>Notification</h1>
+      </div>
+      <div className="middle">
+        <h3>Welcome {username}</h3>
+      </div>
+      <div className="right">
+        <BsBell size={20} />
+        {Boolean(notifs) && <span>{notifs}</span>}
+        <button onClick={logOut}>Logout</button>
+      </div>
     </Container>
   );
 };
